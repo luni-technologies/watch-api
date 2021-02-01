dad4a5439fe06352eac3012ed339c7fffef679d9
Szonja Rozsa <igorzimmermann01@gmail.com>
2021-02-01 13:15:10 +0100

    Movie router works

--- routers/movie.js
+++ routers/movie.js
@@ -5,27 +5,21 @@ const Movie = require('../models/movie')
 
 router.get('/findById/:id', (req, res) => {
   Movie.findOne({publicid:req.params.id})
-    .populate([{path:'meta.genres'},{path:'meta.age_rating'},{path:'cast.person'}])
-    .exec((err, movie) => {
+    .populate(/* [{path:'meta.genres'},{path:'meta.age_rating'},{path:'cast.person'}] */)
+    .then(movie => {
       let respJSON
-      if (!err) {
-        respJSON = {url: req.url, status: 'success', msg: 'Successfully found a movie', movie}
-      } else {
-        respJSON = {url: req.url, status: 'error', msg: err}
-      }
+      respJSON = {url: req.url, status: 'success', msg: 'Successfully found a movie', movie}
+      res.json(respJSON)
     })
 })
 
 router.get('/findByUrl/:url', (req, res) => {
   Movie.findOne({url:req.params.url})
-    .populate([{path:'meta.genres'},{path:'meta.age_rating'},{path:'cast.person'}])
-    .exec((err, movie) => {
+    .populate(/* [{path:'meta.genres'},{path:'meta.age_rating'},{path:'cast.person'}] */)
+    .then(movie => {
       let respJSON
-      if (!err) {
-        respJSON = {url: req.url, status: 'success', msg: 'Successfully found a movie', movie}
-      } else {
-        respJSON = {url: req.url, status: 'error', msg: err}
-      }
+      respJSON = {url: req.url, status: 'success', msg: 'Successfully found a movie', movie}
+      res.json(respJSON)
     })
 })
 
