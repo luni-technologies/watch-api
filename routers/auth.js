db301b5a501252a60019f7770f4e7125e3b6ca7d
Szonja Rozsa <igorzimmermann01@gmail.com>
2021-02-17 19:36:09 +0100

    Login is working

--- routers/auth.js
+++ routers/auth.js
@@ -2,11 +2,13 @@ const express = require('express')
 const router = express.Router()
 const uuid = require('uuid')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/user')
 
 router.post('/register', (req, res) => {
   User.findOne({$or: [{'info.email': req.body.email},{'info.username': req.body.username}]}, (err, doc) => {
-    if (doc) return res.json({url: req.url, status: 'error', msg: 'User already registered'})
+    if (doc) return res.json({url: req.url, status: 'fail', msg: 'User already registered'})
     let userObj = {
       publicid: uuid.v4(),
       info: {
@@ -49,6 +51,22 @@ router.post('/register', (req, res) => {
   })
 })
 
+router.post('/login', (req, res) => {
+  User.findOne({$or: [{'info.email':req.body.userident},{'info.username':req.body.userident}]}, (err, user) => {
+    if (err) return res.json({url: req.url, status: 'error', msg: 'Error while finding user', error: err})
+    if (!user) return res.json({url: req.url, status: 'fail', msg: 'User doesn\'t exist'})
+    bcrypt.compare(req.body.password, user.auth.password, (err, isValid) => {
+      if (err) return res.json({url: req.url, status: 'error', msg: 'Error while comparing hash to password', error: err})
+      if (!isValid) return res.json({url: req.url, status: 'fail', msg: 'Invalid password'})
 
+      const token = jwt.sign(
+        user.toJSON(),
+        process.env.JWT_SECRET
+      )
+
+      res.header('auth-token', token).json({url: req.url, status: 'success', msg: 'Successful login', data: token})
+    })
+  })
+})
 
 module.exports = router
