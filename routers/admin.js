ec1ce4931e3d337fdfa0b0a81e7758d4eab53852
IgorZimmermann <igorzimmermann01@gmail.com>
2021-03-02 12:43:45 +0100

    moved creation routes to admin router

--- routers/admin.js
+++ routers/admin.js
@@ -5,4 +5,80 @@ router.get('/pingAdmin', (req, res) => {
   res.json({url: req.url, status: 'success', msg: 'Pong'})
 })
 
+router.post('/autoFind', async (req, res) => {
+  let url = req.body.imdb
+  let urlList = {
+    home: url,
+    release: url + 'releaseinfo',
+    keywords: url + 'keywords'
+  }
+  let homeresp = await axios.get(urlList.home, {headers: {'Accept-Language': 'en-gb'}})
+  let home$ = cheerio.load(homeresp.data)
+  let releaseresp = await axios.get(urlList.release, {headers: {'Accept-Language': 'en-gb'}})
+  let release$ = cheerio.load(releaseresp.data)
+  let keywordsresp = await axios.get(urlList.keywords, {headers: {'Accept-Language': 'en-gb'}})
+  let keywords$ = cheerio.load(keywordsresp.data)
+
+  let movieObj = {
+    publicid: uuid.v4(),
+    url: home$('.title_wrapper h1').text().trim().split('(')[0].trim().toLowerCase().replace(/ /g, '-'),
+    availability: {
+      date: new Date(),
+      bool: false
+    },
+    meta: {
+      title: {
+        main: home$('.title_wrapper h1').text().trim().split('(')[0].trim(),
+        alternatives: []
+      },
+      plot: home$('.plot_summary .summary_text').text().trim(),
+      storyline: home$('#titleStoryLine .inline.canwrap p span').text().trim(),
+      ratings: {
+        imdb: {
+          number: parseFloat(home$('span[itemprop="ratingValue"]').text()),
+          url: urlList.home
+        }
+      },
+      released: addDays(new Date(release$('.release-date-item__date').eq(0).text()), 1),
+      genres: home$('.subtext').text().split('|')[2].replace(/\n/g, '').trim().split(', '),
+      keywords: keywords$('.sodatext').text().split('\n').map(x => x.trim()).filter(y => y !== '')
+    },
+    cast: []
+  }
+  res.json({url: req.url, status: 'success', msg: 'Auto found title on iMDB', data: movieObj})
+})
+
+router.post('/create', (req, res) => {
+  const movieObj = req.body.movie
+  let newMovie = new Movie(movieObj)
+  newMovie.save().then(() => {
+    console.log(movieObj.url + ' - saved')
+    res.json({url: req.url, status: 'success', msg: 'Saved new title', data: movieObj})
+  }).catch(e => {
+    res.json({url: req.url, status: 'error', msg: 'Error saving new title', error: e})
+  })
+})
+
+router.post('/createManifest/:id', (req, res) => {
+  let manifestObj = {
+    publicid: uuid.v4(),
+    media_type: req.body.mediaType,
+    mediaId: req.params.id,
+    mediaTitle: req.body.mediaTitle,
+    video: {
+      src: `/files/${req.params.id}/media/hls/playlist.m3u8`,
+      lang: req.body.mediaLang,
+      skips: req.body.skips,
+      end: req.body.end
+    },
+    subtitles: req.body.subtitles
+  }
+  let newManifest = new Manifest(manifestObj)
+  newManifest.save().then(() => {
+    res.json({url: req.url, status: 'success', msg: 'Saved new manifest', data: manifestObj})
+  }).catch(e => {
+    res.json({url: req.url, status: 'error', msg: 'Error saving new manifest', error: e})
+  })
+})
+
 module.exports = router
